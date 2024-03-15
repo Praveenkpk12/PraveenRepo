6f9a6ec3321781ac6ee79ac2e89fa9aa5a8f4e82
PraveenKumar491979 <kpk.praveen1997@gmail.com>
2024-03-15 11:21:29 +0530

    Add on

--- src/Git2.java
+++ src/Git2.java
@@ -1,9 +1,13 @@
 
-public class Git2 {
+public class Git2
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Done");
 
 	}
 
 }
+
+
